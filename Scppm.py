e023cf217f897cf6f8ef3675b6c8c2f862f07987
Felix Arnold <felix.arnold@gmail.com>
2018-10-26 00:21:04 +0200

    removed test code

--- Scppm.py
+++ Scppm.py
@@ -18,8 +18,8 @@ class PpmModulator(object):
         self.m_b = int(np.log2(self.m))
 
     def modulate_symbol(self, symbol_num):
-        enc=[0]*(self.m+self.b)
-        enc[symbol_num]=1
+        enc = [0] * (self.m + self.b)
+        enc[symbol_num] = 1
         return enc
 
     def modulate_stream(self, data):
@@ -29,7 +29,7 @@ class PpmModulator(object):
         return symbol.index(max(symbol[0:self.m]))
 
     def demodulate_stream(self, e):
-        return list(flatten(dec2bin(self.demodulate(x), self.m_b) for x in grouped(e, self.m+self.b)))
+        return list(flatten(dec2bin(self.demodulate(x), self.m_b) for x in grouped(e, self.m + self.b)))
 
 
 class ScppmEncoder(object):
@@ -42,28 +42,20 @@ class ScppmEncoder(object):
         self.r = 3
 
     def accu(self, d):
-        trellis_accu = Trellis([[1, 0]],[0, 1] )
+        trellis_accu = Trellis([[1, 0]], [0, 1])
         cve = ConvEncoder(trellis_accu)
         return cve.encode(d, False)
 
-
     def encode(self, d):
         self.cve = ConvEncoder(self.trellis)
-        #print(len(d))
         d = self.cve.encode(d)  # encoding incl zero termination
-        len_x= len(d)
-        #print(len(d))
-        #d=d+[0]*(int(2**(np.ceil(np.log2(len(d))/2)*2))-len(d))
-        d = d + [0]*(len(self.interleaver.perm) - len(d))
-        #print(len(d))
-        d = self.interleaver.interleave(d) # interleaving
+        len_x = len(d)
+        d = d + [0] * (len(self.interleaver.perm) - len(d))
+        d = self.interleaver.interleave(d)  # interleaving
         N_i = len(d)
-        #print("after interleaver: " + str(d))
         d = self.accu(d)
-        #print(len(d))
-        e = self.modulator.modulate_stream(d) # modulation
-        #print(len(e))
-        return [e ,len_x, N_i]
+        e = self.modulator.modulate_stream(d)  # modulation
+        return [e, len_x, N_i]
 
 
 import numpy as np
@@ -72,45 +64,42 @@ import numpy as np
 class PpmTrellis(object):
 
     def __init__(self, modulator):
-
         self.Ns = 2  # number of states
-        self.Nb = 2*modulator.m  # number of branches
+        self.Nb = 2 * modulator.m  # number of branches
         self.r = modulator.m
         self.rsc = 0
         self.wb = int(np.log2(modulator.m))
         self.mod = modulator
         self.get_dat_precalc = []
         self.get_enc_bits_precalc = []
-        self.optimize = False
         self.precalc()
 
     def get_rate(self):
-        return self.gen_matrix.shape[0]
-
-    def get_k(self):
-        return self.gen_matrix.shape[1]
+        return self.r
 
     def get_next_state(self, branch):
-        return int((branch & 2**(self.wb-1)) != 0)
+        return int((branch & 2 ** (self.wb - 1)) != 0)
 
     def get_prev_state(self, branch):
         return int((branch & 2 ** (self.wb)) != 0)
 
     def get_prev_branches(self, state):
-        return np.array([x+ state*2**(self.wb-1) for x in range(2**(self.wb-1))] + [x+state*2**(self.wb-1) + 2**self.wb for x in range(2**(self.wb-1))])
-
+        return np.array([x + state * 2 ** (self.wb - 1) for x in range(2 ** (self.wb - 1))] + [
+            x + state * 2 ** (self.wb - 1) + 2 ** self.wb for x in range(2 ** (self.wb - 1))])
 
     def get_next_branches(self, state):
-        return np.array([x + state * 2 ** (self.wb) for x in range(2 ** (self.wb))]  )
+        return np.array([x + state * 2 ** (self.wb) for x in range(2 ** (self.wb))])
 
     def get_next_branch(self, state, dat):
         print("not implemented")
 
     def get_enc_bits(self, branch):
-        return self.mod.modulate_symbol(branch & (2**self.wb-1))
+        return self.mod.modulate_symbol(branch & (2 ** self.wb - 1))
 
     def get_dat(self, branch):
-        return self._dec2bin((branch & (2 ** self.wb - 1)) ^ ( ((branch & (2**(self.wb-1)-1)) << 1 ) + self.get_prev_state(branch)), self.wb)
+        return self._dec2bin(
+            (branch & (2 ** self.wb - 1)) ^ (((branch & (2 ** (self.wb - 1) - 1)) << 1) + self.get_prev_state(branch)),
+            self.wb)
 
     def _dec2bin(self, val, k):
         bin_val = []
@@ -120,12 +109,6 @@ class PpmTrellis(object):
         return bin_val
 
     def precalc(self):
-        self.get_dat_pc=[]
-        self.get_enc_bits_pc = []
-        for i in range(self.Nb):
-            self.get_dat_pc.append(self.get_dat(i))
-            self.get_enc_bits_pc.append(self.get_enc_bits(i))
-
         self.get_dat_pc = [self.get_dat(x) for x in range(self.Nb)]
         self.get_enc_bits_pc = [self.get_enc_bits(x) for x in range(self.Nb)]
         self.get_next_state_pc = [self.get_next_state(x) for x in range(self.Nb)]
@@ -133,7 +116,6 @@ class PpmTrellis(object):
         self.get_next_branches_pc = [self.get_next_branches(x) for x in range(self.Ns)]
         self.get_prev_branches_pc = [self.get_prev_branches(x) for x in range(self.Ns)]
 
-        self.optimize = True
 
 # -------------------- helper functions ---------------------------------
 
@@ -146,6 +128,7 @@ def grouped(seq, n):
             return
         yield chunk
 
+
 def dec2bin(val, k):
     bin_val = []
     for j in range(k):
@@ -153,11 +136,13 @@ def dec2bin(val, k):
         val = val >> 1
     return bin_val
 
+
 def bin2dec(bin_val):
     n = len(bin_val)
     int_val = 0
     for j in range(n):
         int_val = int_val + bin_val[j] * 2 ** j
     return int_val
+
 
 flatten = lambda l: (item for sublist in l for item in sublist)
--- ScppmTest.py
+++ ScppmTest.py
@@ -6,32 +6,32 @@ from turpy.Interleaver import Interleaver
 from turpy.SisoDecoder import SisoDecoder
 from ScppmDecoder import ScppmDecoder
 
-n_data = 1760 #7560
+n_data = 1760  # 7560
 
 # create trellises, encoders and decoders instances
 gp = [[1, 0, 1], [1, 1, 1]]
 trellis = Trellis(gp)
 
 # ppm modulator
-m= 16
-b= 0
+m = 16
+b = 0
 ppm_mod = Scppm.PpmModulator(m, b)
 
 # sisos
 ppm_trellis = Scppm.PpmTrellis(ppm_mod)
-inner_siso =SisoDecoder(ppm_trellis) #Scppm.ConvSISOMultiBit(ppm_trellis)
+inner_siso = SisoDecoder(ppm_trellis)
 
 outer_siso = SisoDecoder(trellis)
 outer_siso.backward_init = True
 
 # create interleaver instances
-len_e = trellis.r*n_data
-print(len_e)
-n_i = len_e #int(2**(np.ceil(np.log2(len_e)/2)*2))
+len_e = trellis.r * n_data
+
+n_i = len_e
 il = Interleaver()
 assert (n_i % ppm_mod.m_b) == 0
-#assert n_i == 15120
-#il.gen_qpp_perm_poly(n_i, 11, 210)
+# assert n_i == 15120
+# il.gen_qpp_perm_poly(n_i, 11, 210)
 il.gen_rand_perm(n_i)
 
 # create decoder
@@ -40,50 +40,47 @@ scppm_decoder.iterations = 20
 
 # generate data
 data_u = list(((rand(n_data) >= 0.5).astype(int)))
-data_u[-1]=0
-data_u[-2]=0 # zero termination
+data_u[-1] = 0
+data_u[-2] = 0  # zero termination
 
 # encode
-scppm_enc = Scppm.ScppmEncoder(trellis, il,ppm_mod)
+scppm_enc = Scppm.ScppmEncoder(trellis, il, ppm_mod)
 [c, len_x, n_i2] = scppm_enc.encode(data_u)
 assert n_i == n_i2
 
 # noise
-#EbNodB = 2.6
-#EbNo = 10.0 ** (EbNodB / 10.0)
-#print(EbNo)
-#noise_std = float(np.sqrt(2) / np.sqrt(2 * EbNo))
-#y = [16 * x + noise_std * float(randn(1)) for x in c]
+# EbNodB = 2.6
+# EbNo = 10.0 ** (EbNodB / 10.0)
+# print(EbNo)
+# noise_std = float(np.sqrt(2) / np.sqrt(2 * EbNo))
+# y = [16 * x + noise_std * float(randn(1)) for x in c]
 
 # poisson noise
 lambda_n = 0.2
 lambda_s_dB = 2.9
 lambda_s = 10.0 ** (lambda_s_dB / 10.0)
-y=[]
+y = []
 for x in c:
-    if x>0:
+    if x > 0:
         ys = np.random.poisson(lambda_s)
     else:
         ys = 0
     yn = np.random.poisson(lambda_n)
-    y.append(ys+yn)
+    y.append(ys + yn)
 
-ll=[]
+ll = []
 for x in y:
-    lh=(((lambda_s+lambda_n)**x) * np.exp(-lambda_s) ) / (lambda_n**x)
+    lh = (((lambda_s + lambda_n) ** x) * np.exp(-lambda_s)) / (lambda_n ** x)
     ll.append(np.log(lh))
 
 y_sparse = []
-for ppm_sym in Scppm.grouped(y, (ppm_mod.m+ppm_mod.b)):
-    index_max=ppm_sym.index(max(ppm_sym))
-    empty_symbol = [0]*(ppm_mod.m+ppm_mod.b)
-    empty_symbol[index_max]= ppm_sym[index_max]
+for ppm_sym in Scppm.grouped(y, (ppm_mod.m + ppm_mod.b)):
+    index_max = ppm_sym.index(max(ppm_sym))
+    empty_symbol = [0] * (ppm_mod.m + ppm_mod.b)
+    empty_symbol[index_max] = ppm_sym[index_max]
     y_sparse.extend(empty_symbol)
 
 # decode
 [u, ber_iter] = scppm_decoder.decode(ll, n_data, n_i, len_x, data_u)
 
 print("ber_iter= " + str(ber_iter))
-
-
-
